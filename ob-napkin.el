01bf62d19ae1e6b89e68c611be407c681c715056
Hans Jang <hsjang8848@gmail.com>
2020-08-04 18:25:23 +1000

    Clean up before MELPA submit

--- ob-napkin.el
+++ ob-napkin.el
@@ -1,11 +1,12 @@
-;;; ob-napkin.el --- org-babel functions for template evaluation
+;;; ob-napkin.el --- Babel functions for Napkin -*- lexical-binding: t -*-
 
 ;; Copyright (C) Hans Jang
 
 ;; Author: Hans Jang
-;; Keywords: literate programming, reproducible research
-;; Homepage: https://orgmode.org
-;; Version: 0.5
+;; Keywords: literate programming, reproducible research, napkin, plantuml
+;; Homepage: https://github.com/pinetr2e/ob-napkin
+;; Version: 0.9
+;; Package-Requires: ((emacs "26.1"))
 
 ;;; License:
 
@@ -24,30 +25,31 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
-;;; Commentary:
+;; This file is not part of GNU Emacs.
 
+;;; Commentary:
 
 ;; Org-Babel support for evaluating Napkin script, a Python based DSL to write
 ;; sequence diagrams.
-;;
 
 ;;; Requirements:
 
 ;;
 ;; pip install napkin plantuml
 ;;
+;; See https://github.com/pinetr2e/napkin
 
 ;;; Code:
 (require 'ob)
 
 (defcustom org-babel-napkin-command "napkin"
-  "Name of the command for running napkin command line"
+  "Name of the command for running napkin tool command line."
   :group 'org-babel
   :type 'string)
 
 (defcustom org-babel-napkin-plantuml-server-url ""
-  "Server URL to use in generating image file. The empty string
-means to use the public server."
+  "Server URL to use in generating image file.
+The empty string means to use the public server."
   :group 'org-babel
   :type 'string)
 
@@ -56,20 +58,23 @@ means to use the public server."
   "Default arguments for evaluating a napkin src block.")
 
 (defun org-babel-napkin-out-file (params)
+  "Return output file name from PARAMS."
   (or (cdr (assq :file params))
-      (error "napkin src block requires :file header argument.")))
+      (error "Napkin src block requires :file header argument")))
 
 (defun org-babel-expand-body:napkin (body params)
-  (let* ((out-file (org-babel-napkin-out-file params))
-         (vars (org-babel--get-vars params)))
+  "Expand BODY according to PARAMS, return the expanded body."
+  (let ((out-file (org-babel-napkin-out-file params)))
     (if (string-match (rx buffer-start (0+ blank) "def" (1+ blank) "seq_diagram" (0+ blank) "(") body)
         (concat "import napkin\n"
                 (format "@napkin.seq_diagram('%s')\n" (file-name-base (org-babel-process-file-name out-file)))
                 body)
-      (error "napkin src block requires def seq_diagram() as the first line of the contents"))))
+      (error "Napkin src block requires def seq_diagram() as the first line of the contents"))))
 
 
 (defun org-babel-execute:napkin (body params)
+  "Execute a block of napkin code with BODY and PARAMS with Babel.
+This function is called by `org-babel-execute-src-block'."
   (let* ((out-file (org-babel-napkin-out-file params))
          (img-type (file-name-extension out-file))
          (in-file (org-babel-temp-file "napkin-" ".py"))
@@ -80,7 +85,7 @@ means to use the public server."
                           (org-babel-process-file-name in-file)
                           (file-name-directory (org-babel-process-file-name out-file))
                           (concat "plantuml_" img-type)
-                          (if (string-empty-p server-url) "" (concant "--server-url" server-url)))))
+                          (if (string-equal server-url "") "" (concat "--server-url " server-url)))))
     (with-temp-file in-file (insert expanded-body))
     (message "%s" command)
     (org-babel-eval command "")
